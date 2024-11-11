ccc3f0683a785f3d5e2764e20320e54b5f7b62f6
Thomas Detlefsen <ted32@pitt.edu>
2024-11-10 20:14:05 -0500

    Outline for training

--- crop_weed_discrim/trainer.py
+++ crop_weed_discrim/trainer.py
@@ -1,1 +1,21 @@
-# TODO: Function to train the model based on parameters from train.py
+# TODO: Add terminal output for important things (acc, loss, etc.)
+
+def train():
+    '''
+    Train the model based on specified training parameters
+
+    Args:
+        <ARG> (<TYPE>): <INFORMATION>
+
+    Returns:
+        <RETURN> (<TYPE>): <INFORMATION>
+    '''
+
+    # TODO: Create a new experiment under the exp directory (unique name, save hyperparameters, tensorboard)
+
+    # TODO: Loop through epochs
+        # TODO: Train model
+        # TODO: Validate model (every N epochs)
+        # TODO: Save results
+
+    pass
--- train.py
+++ train.py
@@ -1,3 +1,20 @@
 from crop_weed_discrim import trainer
 
-# TODO: Train model based on config.yaml
+# TODO: Add terminal output for important things (hyperparams)
+
+if __name__ == "__main__":
+    # TODO: Set random seeds
+
+    # TODO: Load in parameters from config.yaml
+
+    # TODO: Initialize model
+
+    # TODO: Initialize optimizer and scheduler
+
+    # TODO: Load in dataset + augmentations
+
+    # TODO: Setup loss function
+
+    # TODO: Train model using trainer.py
+
+    pass
