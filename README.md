ef9d3ec8ac9a50db80c3fb7621ddd2664a340e95
Thomas Detlefsen <ted32@pitt.edu>
2024-11-21 17:55:29 -0500

    README.md, installation, and testing

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# crop-weed-discrim
+# Edible Carrot or Poisonous Hemlock?
 A crop-weed discrimination project for 16-824 Visual Learning &amp; Recognition
+
+## Installation
+Install the required python packages:
+
+```
+pip install -r requirements.txt
+```
+
+## Training
+To train a new model, modify the hyperparameters in [config.yaml](config.yaml) and run the following command:
+
+```
+python train.py
+```
+
+This will create a new experiment in the `exp` directory containing:
+- `config.yaml` - A copy of the `config.yaml` used for training
+- `events.out.tfevents` - A log of training statistics viewable via tensorboard
+- `weights.pth` - The weights from the best performing epoch
+- `confusion_matrix.png` - A confusion matrix run on the test data
+
+## Testing
+To test a previously trained model, run the following command where `N` is the experiment you would like to test.
+
+```
+python test.py exp<N>
+```
+
+This will return the accuracy of the model and overwrite `confusion_matrix.png`, the confusion matrix run on the test data. Testing will use the `config.yaml` specified in the experiment
---
+++ test.py
@@ -0,0 +1,80 @@
+import os
+import yaml
+import random
+import argparse
+import numpy as np
+
+import torch
+from torchvision import transforms
+
+from crop_weed_discrim import trainer, tester
+from crop_weed_discrim.utils.utils import ARGS
+from crop_weed_discrim.models.resnet_cnn import ResNetCNN
+from crop_weed_discrim.utils.dataloader import PlantSeedlingsDataset, SubsetWrapper
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment', metavar='DIRECTORY', help="Experiment to run test on")
+    args, opts = parser.parse_known_args()
+
+    if args.experiment == None:
+        raise Exception("No experiment specified")
+    
+    experiment_dir = os.path.join("exp", args.experiment)
+
+    if not os.path.exists(experiment_dir):
+        raise Exception("Experiment path not found")
+
+    # Set random seeds
+    np.random.seed(0)
+    torch.manual_seed(0)
+    random.seed(0)
+
+    # Load in parameters from config.yaml
+    with open(os.path.join(experiment_dir, "config.yaml"), "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    configs = ARGS(
+        model_name = config['model']['name'],
+        dataset_name = config['dataset']['name'],
+        epochs = config['hyperparams']['epochs'],
+        batch_size = config['hyperparams']['batch_size'],
+        lr = config['hyperparams']['lr'],
+        step_size = config['hyperparams']['step_size'],
+        gamma = config['hyperparams']['gamma'],
+        use_cuda = config['hyperparams']['use_cuda'],
+        val_every = config['hyperparams']['val_every']
+    )
+
+    print("Loaded config.yaml:")
+    print(configs)
+
+    # Load dataset
+    datasets = ["plant_seedlings"]
+    if configs.dataset_name == "plant_seedlings":
+        dataset = PlantSeedlingsDataset()
+    else:
+        raise NotImplementedError(f"{configs.dataset_name} not implemented, choose one of the following {datasets}")
+    
+    # Create Augmentations
+    tfs = [
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(degrees=45),
+        transforms.ColorJitter(),
+    ]
+
+    # Split into train, val, test
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, [round(0.8 * len(dataset)), round(0.1 * len(dataset)), round(0.1 * len(dataset))])
+    data = {"train": SubsetWrapper(train_set, tfs=tfs),
+            "val": SubsetWrapper(val_set),
+            "test": SubsetWrapper(test_set)}
+
+    # Initialize model
+    model = ResNetCNN(len(dataset.class_names))
+
+    # Load model
+    model.load_state_dict(torch.load(os.path.join('exp', config.experiment.name, 'best.pth'), map_location=configs.device))
+    model = model.to(configs.device)
+
+    # Test model using tester.py
+    tester.test(configs, data, model, experiment_dir, dataset.class_names)
